5b00c82ab213a2eea92e9d75b7b84b824784b4cd
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-07 15:20:40 +0530

    Implement gameApp to execute fecthGames functionality

--- src/gameLibraryApp.ts
+++ src/gameLibraryApp.ts
@@ -1,1 +1,10 @@
-console.log("Welcome to Game library management....")
+import { fetchGames } from "./data/fetchGames";
+import * as dotenv from "dotenv";
+import { Game } from "./types/gameType";
+dotenv.config()
+
+const API_URL:string=process.env.API_URL as string;
+async function gameApp(){
+    const games:Game[]=await fetchGames(API_URL)
+}
+gameApp()
