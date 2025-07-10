68129f4df9b43438c28cf936e67b4d911b38d4f4
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-10 11:40:05 +0530

    Add tests to verify switch case operaions working

--- tests/services/adminFunctionalities.test.ts
+++ tests/services/adminFunctionalities.test.ts
@@ -1,18 +1,33 @@
 import { adminFunctionalities } from "../../src/services/adminFunctionalities";
+import { selectRole } from "../../src/services/selectRole";
 import { Game } from "../../src/types/gameType";
+import { addGameByAdmin } from "../../src/utils/addGameByAdmin";
+jest.mock("../../src/utils/addGameByAdmin")
+jest.mock("../../src/services/selectRole")
 describe("admin Functionalities",()=>{
     let consoleLogSpy:jest.SpyInstance;
+    let mockGames:Game[];
+    let promptMock=jest.fn();
     beforeEach(()=>{
-        jest.clearAllMocks;
+        jest.clearAllMocks();
         consoleLogSpy=jest.spyOn(console,"log").mockImplementation(()=>{})
+        mockGames=[]
     })
-    test("test console log called correctly",()=>{
-        const mockGames:Game[]=[]
-        adminFunctionalities(mockGames,()=>"admin")
-        expect(consoleLogSpy).toHaveBeenCalledWith("---------Hey Admin, Please select (1-4)----------")
-        expect(consoleLogSpy).toHaveBeenCalledWith("1. Add Game")
-        expect(consoleLogSpy).toHaveBeenCalledWith("2. Remove Game")
-        expect(consoleLogSpy).toHaveBeenCalledWith("3. View All Games")
-        expect(consoleLogSpy).toHaveBeenCalledWith("4. Back")
+    test("test addGame is called or not",()=>{
+        promptMock.mockReturnValueOnce("1")
+        adminFunctionalities(mockGames,promptMock);
+        expect(addGameByAdmin).toHaveBeenCalledWith(mockGames,promptMock)
+    })
+    test("test addGame is called or not",()=>{
+        promptMock.mockReturnValueOnce("4")
+        adminFunctionalities(mockGames,promptMock);
+        expect(selectRole).toHaveBeenCalled()
+    })
+    test("test invalid input for selecting operation",()=>{
+        promptMock.mockReturnValueOnce("invalid")
+        .mockReturnValueOnce("1")
+        adminFunctionalities(mockGames,promptMock);
+        expect(consoleLogSpy).toHaveBeenCalledWith("Invalid Input! Please enter valid option")
+        expect(addGameByAdmin).toHaveBeenCalled()
     })
 })
