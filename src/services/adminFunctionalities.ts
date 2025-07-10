5541ded41de2cb210edcf93e7623c4be3eaeecd7
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-10 11:39:03 +0530

    Refactor adminFunctionality to add switch operations

--- src/services/adminFunctionalities.ts
+++ src/services/adminFunctionalities.ts
@@ -1,9 +1,24 @@
 import { Game } from "../types/gameType";
+import { addGameByAdmin } from "../utils/addGameByAdmin";
+import { selectRole } from "./selectRole";
 
-export function adminFunctionalities(games:Game[],prompt:(msg:string)=>string):void{
+export function adminFunctionalities(games:Game[],prompt:(msg:string)=>string):any{
     console.log("---------Hey Admin, Please select (1-4)----------")
     console.log("1. Add Game")
     console.log("2. Remove Game")
     console.log("3. View All Games")
     console.log("4. Back")
+    const input:string=prompt("Select operation from (1-4)");
+    const operation=input.toLowerCase()
+    switch(operation){
+        case "1":
+            addGameByAdmin(games,prompt)
+            break;
+        case "4": 
+            selectRole(games,prompt)
+            break;
+        default:
+            console.log("Invalid Input! Please enter valid option")
+            adminFunctionalities(games,prompt)
+    }
 }
