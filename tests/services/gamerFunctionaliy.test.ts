cd736a444b7548b6934d345dcfceba8acfe7a513
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-14 17:51:43 +0530

    Add tests to verify switch operations

---
+++ tests/services/gamerFunctionaliy.test.ts
@@ -0,0 +1,50 @@
+import { gamerFunctionalities } from "../../src/services/gamerFunctionalities";
+import { searchGameByGamer } from "../../src/utils/searchGameByGamer";
+import { selectRole } from "../../src/services/selectRole";
+import { Game } from "../../src/types/gameType";
+
+jest.mock("../../src/utils/searchGameByGamer");
+jest.mock("../../src/services/selectRole");
+
+describe("gamerFunctionality",()=>{
+    let logSpy:jest.SpyInstance;
+    let promptMock=jest.fn();
+    let games:Game[];
+
+    beforeEach(()=>{
+        logSpy=jest.spyOn(console,"log").mockImplementation(()=>{})
+        games=[]
+    })
+
+    test("should call searchGameByAdmin on input '1'",()=>{
+        promptMock.mockReturnValueOnce("1")
+        gamerFunctionalities(games,promptMock);
+        expect(searchGameByGamer).toHaveBeenCalledWith(games,promptMock)
+    })
+
+    test("should call searchGameByAdmin on input 'search game'",()=>{
+        promptMock.mockReturnValueOnce("search game")
+        gamerFunctionalities(games,promptMock);
+        expect(searchGameByGamer).toHaveBeenCalledWith(games,promptMock)
+    })
+    
+    test("should return to previous section on iput '5'",()=>{
+        promptMock.mockReturnValueOnce("5");
+        gamerFunctionalities(games,promptMock);
+        expect(selectRole).toHaveBeenCalledWith(games,promptMock);
+    })
+    test("should return to previous section on back",()=>{
+        promptMock.mockReturnValueOnce("back");
+        gamerFunctionalities(games,promptMock);
+        expect(selectRole).toHaveBeenCalledWith(games,promptMock);
+    })
+
+    test("should prompt again if invalid input given",()=>{
+        promptMock.mockReturnValueOnce("chandu")
+        .mockReturnValueOnce("1")
+        gamerFunctionalities(games,promptMock);
+        expect(logSpy).toHaveBeenCalledWith("Invalid input! Please enter valid input.")
+        expect(searchGameByGamer).toHaveBeenCalled()
+    })
+   
+})
