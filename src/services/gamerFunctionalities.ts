9bf37d4f6935b80ae4a00d7285423762d5b4ea1c
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-14 17:51:05 +0530

    Implement switch case for gamer functionality

---
+++ src/services/gamerFunctionalities.ts
@@ -0,0 +1,29 @@
+import { Game } from "../types/gameType";
+import { searchGameByGamer } from "../utils/searchGameByGamer";
+import { selectRole } from "./selectRole";
+
+export function gamerFunctionalities(games:Game[],prompt:(msg:string)=>string){
+    console.log("------ Hey Gamer, Please select operation ------")
+    console.log("1. Search Game");
+    console.log("2. Filter Games");
+    console.log("3. Add to Wishlist")
+    console.log("4. View Wishlist");
+    console.log("5. Back")
+    const operation:string=prompt("Please select option from (1-5):")
+
+    switch(operation){
+        case "1":
+        case "search game":
+            searchGameByGamer(games,prompt);
+            break;
+
+        case "5":
+        case "back":
+            selectRole(games,prompt);
+            break;
+
+        default:
+            console.log("Invalid input! Please enter valid input.");
+            gamerFunctionalities(games,prompt)
+    }
+}
