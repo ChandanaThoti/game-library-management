ddb8e5330e2f8c82fbeb942bb8b7a7dc8e3275c2
Thoti Chandana <thotichandana@admins-MacBook-Pro.local>
2025-07-15 17:57:51 +0530

    Add export to wishList array

--- src/utils/addToWishlistByGamer.ts
+++ src/utils/addToWishlistByGamer.ts
@@ -1,7 +1,7 @@
 import { gamerFunctionalities } from "../services/gamerFunctionalities";
 import { Game } from "../types/gameType";
 
-let wishList:Game[]=[]
+export let wishList:Game[]=[]
 export function addToWishlistByGamer(games:Game[],prompt:(msg:string)=>string):void{
     const gameInput:string=prompt("Enter game name or Id to addToWishlist:");
     const existingGame:Game |undefined=games.find(game=> game.name.toLowerCase()===gameInput.toLowerCase() || game.id.toString()===gameInput);
